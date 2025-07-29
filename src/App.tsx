f1e17d9428de338f521ff315d27985b98a574380
Syscaller <21367575+syscaller-dev@users.noreply.github.com>
2025-07-29 16:41:35 +0200

    initial commit

---
+++ src/App.tsx
@@ -0,0 +1,18 @@
+
+import TrashtalkGenerator from "./TrashtalkGenerator";
+function App() {
+  return (
+    <div style={{
+      minHeight: '100vh',
+      minWidth: '100vw',
+      display: 'flex',
+      alignItems: 'center',
+      justifyContent: 'center',
+      background: '#181a20',
+    }}>
+      <TrashtalkGenerator />
+    </div>
+  );
+}
+
+export default App;
